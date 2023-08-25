d8bcdc157165c1c434aa5fa135c74bba8ab47331
Eli JuarezR <19680175@cuautla.tecnm.mx>
2023-08-25 10:58:25 -0600

    inicializacion

---
+++ src/main/java/com/naatik/ich/security/IchSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.naatik.ich.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IchSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IchSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/naatik/ich/security/IchSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naatik.ich.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IchSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
